ecabe31b93c4a92e8b1a11b63a076bbcd9539cce
hoenybears <clone0922@gmail.com>
2026-03-26 17:00:52 +0900

    wip

---
+++ src/test/java/com/mada/server/ServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mada.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+class ServerApplicationTests {
+
+    @Test
+    void writeDocumentationSnippets() {
+
+        var modules = ApplicationModules.of(ServerApplication.class).verify();
+
+        new Documenter(modules)
+            .writeModulesAsPlantUml()
+            .writeIndividualModulesAsPlantUml();
+    }
+}
